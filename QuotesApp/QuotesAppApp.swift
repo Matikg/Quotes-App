742f7ef94ffd8ab687f38388ba74f30668c7eeea
Mateusz <matikg@interia.eu>
2024-07-03 16:06:17 +0200

    Initial Commit

---
+++ QuotesApp/QuotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesAppApp.swift
+//  QuotesApp
+//
+//  Created by Mateusz Grudzień on 03/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct QuotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
